83d254b102c381d02a4e5ad4c685ed58227ffe58
Ikinidae <federicaelia24@gmail.com>
2022-05-03 15:46:18 +0200

    Progetto finito davvero

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const nome = prompt ("Qual è il tuo nome?")
-console.log(nome)
+const nome = prompt ("Qual è il tuo nome?");
+console.log(nome);
 
-const cognome = prompt ("Qual è il tuo cognome?")
-console.log(cognome)
+const cognome = prompt ("Qual è il tuo cognome?");
+console.log(cognome);
 
-const colore = prompt ("Qual è il tuo colore preferito?")
-console.log(colore)
+const colore = prompt ("Qual è il tuo colore preferito?");
+console.log(colore);
 
-document.getElementById("output").innerHTML = `${nome} ${cognome} ${colore} 21`
+document.getElementById("output").innerHTML = `${nome}${cognome}${colore}21`;
